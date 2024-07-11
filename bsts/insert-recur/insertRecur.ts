70c3fbc06188ff8ee56e74feb228ec722843140e
Anya Aven <leafeon96@gmail.com>
2024-07-11 19:37:37 -0400

    Finish insertRecur - Recursively

--- bsts/insert-recur/insertRecur.ts
+++ bsts/insert-recur/insertRecur.ts
@@ -1,9 +1,34 @@
-import { BSTNum } from "../common/bst";
+import { BNode, BSTNum } from "../common/bst";
 
 /** insertRecursively(val): Insert a new node into the BST with value val.
  * Uses recursion. */
 
 function insertRecur(bst: BSTNum, val: number): void {
+  if (!bst.root) {
+    bst.root = new BNode(val);
+    return;
+  }
+
+  if (bst.root.val === val) return;
+
+  if (val < bst.root.val) {
+    // go left
+    if (!bst.root.left) {
+      bst.root.left = new BNode(val);
+      return;
+    }
+    const leftBST = new BSTNum(bst.root.left)
+    insertRecur(leftBST, val);
+  } else {
+    // go right
+    if (!bst.root.right) {
+      bst.root.right = new BNode(val);
+      return;
+    }
+
+    const rightBST = new BSTNum(bst.root.right)
+    insertRecur(rightBST, val);
+  }
 }
 
 export { insertRecur };
